0bf8ba9ab36ca7874fd7ca2a13d93eb034d706e1
1004682390@qq.com <wxj121987>
2019-06-11 23:17:52 +0800

    coap初始搭建

---
+++ src/main/java/com/colmoon/coap/core/network/Endpoint.java
@@ -0,0 +1,12 @@
+package com.colmoon.coap.core.network;
+
+/**
+ * @ClassName Endpoint
+ * @Description TODO
+ * @Author wuxiaojian
+ * @Date 2019/6/11 23:12
+ * @Version 1.0
+ **/
+
+public interface Endpoint {
+}
---
+++ src/main/java/com/colmoon/coap/core/server/resources/Resource.java
@@ -0,0 +1,13 @@
+package com.colmoon.coap.core.server.resources;
+
+/**
+ * @ClassName Resource
+ * @Description TODO
+ * @Author wuxiaojian
+ * @Date 2019/6/11 23:06
+ * @Version 1.0
+ **/
+
+public interface Resource {
+
+}
